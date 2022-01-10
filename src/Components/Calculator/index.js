7e1abce1b3b9bc5e1e7e030c1b3ed6a325d90afd
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-01-10 15:29:01 -0300

    get key on array

--- src/Components/Calculator/index.js
+++ src/Components/Calculator/index.js
@@ -1,33 +1,104 @@
 import styles from './styles.module.scss';
+import { useState } from 'react';
+
 
 export default function Calculator(){
+
+    const [selectedValue, setSelectedValue] = useState([]);
+
+
+    function handleInput(e) {
+      const currentValue = e.target.value;
+      setSelectedValue(oldArray => [...oldArray, currentValue])
+      console.log(selectedValue);
+    }
+
+    function makeCalc() {
+      console.log("calculating...")
+    }
+
+
   return(
     <div className={styles.calculatorContainer}>
-      <input type="text" className={styles.screenContainer} value="0" disabled/>
+      <input 
+        type="text"
+        onClick={handleInput}
+        className={styles.screenContainer} 
+        value="0" 
+        disabled
+      />
 
       <div className={styles.keysContainer}>
-        <button className={styles.operatorContainer} type="button" value="+">+</button>
-        <button className={styles.operatorContainer} type="button" value="-">-</button>
-        <button className={styles.operatorContainer} type="button" value="*">*</button>
-        <button className={styles.operatorContainer} type="button" value="/">/</button>
+        <button 
+          className={styles.operatorContainer} 
+          onClick={handleInput}
+          type="button" 
+          value="+">
+          +
+        </button>
 
-        <button type="button" value="7">7</button>
-        <button type="button" value="8">8</button>
-        <button type="button" value="9">9</button>
+        <button 
+          className={styles.operatorContainer} 
+          onClick={handleInput} 
+          type="button" 
+          value="-">
+          -
+        </button>
 
-        <button type="button" value="4">4</button>
-        <button type="button" value="5">5</button>
-        <button type="button" value="6">6</button>
+        <button 
+          className={styles.operatorContainer} 
+          onClick={handleInput} 
+          type="button" 
+          value="*">
+          *
+        </button>
 
-        <button type="button" value="1">1</button>
-        <button type="button" value="2">2</button>
-        <button type="button" value="3">3</button>
+        <button 
+          className={styles.operatorContainer} 
+          onClick={handleInput} 
+          type="button" 
+          value="/">
+          /
+        </button>
 
-        <button type="button" value="0">0</button>
-        <button className={styles.decinalButton} type="button" value=".">.</button>
-        <button className={styles.allClearButton} type="button" value="all-clear">AC</button>
+        <button type="button" onClick={handleInput} value="7">7</button>
+        <button type="button" onClick={handleInput} value="8">8</button>
+        <button type="button" onClick={handleInput} value="9">9</button>
+
+        <button type="button" onClick={handleInput} value="4">4</button>
+        <button type="button" onClick={handleInput} value="5">5</button>
+        <button type="button" onClick={handleInput} value="6">6</button>
+
+        <button type="button" onClick={handleInput} value="1">1</button>
+        <button type="button" onClick={handleInput} value="2">2</button>
+        <button type="button" onClick={handleInput} value="3">3</button>
+
+        <button type="button"onClick={handleInput} value="0">0</button>
+
+        <button 
+          className={styles.decinalButton} 
+          onClick={handleInput} 
+          type="button" 
+          value=".">
+          .
+        </button>
+
+        <button 
+          className={styles.allClearButton}
+          onClick={handleInput}
+          type="button"
+          value="all-clear">
+          AC
+        </button>
         
-        <button className={styles.equalSignButton} type="button" value="=">=</button>
+        <button 
+          className={styles.equalSignButton}
+          onClick={handleInput}
+          // onClick={makeCalc}
+          type="button" 
+          value="=">
+          =
+        </button>
       </div>
     </div>
   );
